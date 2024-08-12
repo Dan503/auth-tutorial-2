329dc984edc85667da24e41985434a29baebd84f
Daniel Tonon <dan503.npm@gmail.com>
2024-08-12 12:38:10 +1000

    created nav bar

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -1,9 +1,33 @@
+'use client'
+import { usePathname } from 'next/navigation'
 import { AuthButton } from './AuthButton'
+import Link from 'next/link'
+import s from './NavMenu.module.css'
 
 export function NavMenu() {
+	const pathname = usePathname()
+	const checkIsCurrentPage = (comparisonPath: string) =>
+		pathname === comparisonPath ? 'page' : undefined
+
 	return (
-		<nav>
+		<nav className={s.nav}>
 			<AuthButton />
+			<hr />
+			<ul>
+				<li>
+					<Link href="/" aria-current={checkIsCurrentPage('/')}>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="/protected"
+						aria-current={checkIsCurrentPage('/protected')}
+					>
+						Protected route
+					</Link>
+				</li>
+			</ul>
 		</nav>
 	)
 }
